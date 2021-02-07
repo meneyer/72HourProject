e06b2c169451268b25310ae7e2b91db1377ded73
Ginger Alford origins <Ginger@Gingers-MacBook-Pro.local>
2021-02-07 16:14:15 -0500

    moved weather fetch into main App.js file

--- src/App.js
+++ src/App.js
@@ -7,9 +7,12 @@ import {
 } from "react-router-dom";
 
 function App() {
+  document.body.style = 'background: lightgrey;';
   const [address, setAddress] = useState('');
   const [latitude, setLatitude] = useState('');
   const [longitude, setLongitude] = useState('');
+  const [weather, setWeather] = useState({});
+  const apiKey = "43eb2da9b4bf5ad4c050711b4732c0b0"
 
   const lookupLocation = () => {
     if (window.navigator.geolocation) {
@@ -25,6 +28,15 @@ function App() {
     const { latitude, longitude } = position.coords;
     setLatitude(latitude);
     setLongitude(longitude);
+    console.log(latitude, longitude);
+    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`)
+        .then((res) => res.json())
+        .then((json) => {
+            console.log(json)
+            setWeather(json)
+            
+        })
+        ;
     // console.log(latitude, longitude);
     // fetch(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=50dfb7d1bf6f497db2e4d36090ffa01c`)
     // .then((res) => res.json())
@@ -33,6 +45,19 @@ function App() {
     //   });
       
   };
+
+  // function fetchWeather(){
+  //   // if (props.latitude !== "" && props.longitude !== "") {
+  //       fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`)
+  //       .then((res) => res.json())
+  //       .then((json) => {
+  //           console.log(json)
+  //           setWeather(json)
+            
+  //       })
+  //       ;
+    
+  // };
 
   useEffect(() => {
     lookupLocation();
@@ -43,8 +68,9 @@ function App() {
   return (
     <div>
       <Router>
-        <Sitebar address={address} latitude={latitude} longitude={longitude} />
+        <Sitebar address={address} latitude={latitude} longitude={longitude} weather={weather}/>
         {/* <button onClick={lookupLocation()}>Get Location</button> */}
+        
       </Router>
     </div>
   );
