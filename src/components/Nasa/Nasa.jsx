05138be43e284b32b426ba913602d3fe8d3361c4
Aaron <50504602+xAaronWx@users.noreply.github.com>
2021-02-08 20:27:41 -0500

    Image displays

--- src/components/Nasa/Nasa.jsx
+++ src/components/Nasa/Nasa.jsx
@@ -1,38 +1,40 @@
 import React, { useState } from "react";
 
 const Nasa = (props) => {
-  const current = new Date();
-  const date = `${current.getFullYear()}-${
-    current.getMonth() + 1
-  }-${current.getDate()}`;
+  // const current = new Date();
+  // const date = `${current.getFullYear()}-${
+  //   current.getMonth() + 1
+  // }-${current.getDate()}`;
 
   const baseUrl = "https://api.nasa.gov/planetary/earth/assets";
-  // let date = "2018-01-01";
-  const dim = "0.15";
+  let date = "2020-05-07";
+  const dim = "0.3";
   const key = "AlbKXiOG2eTvjRE5caNQzzpVU4sDRUZZOpV8KaD8";
-  // let url = `${baseUrl}?lon=${props.longitude}&lat=${props.latitude}&date=${date}&dim=${dim}&api_key=${key}`;
-  let url = `${baseUrl}?lon=-95.33&lat=29.78&date=2018-01-01&dim=${dim}&api_key=${key}`;
+  let url = `${baseUrl}?lon=${props.longitude}&lat=${props.latitude}&date=${date}&dim=${dim}&api_key=${key}`;
 
   // TEMP ADDRESS https://api.nasa.gov/planetary/earth/imagery?lon=-95.33&lat=29.78&date=2018-01-01&dim=0.15&api_key=DEMO_KEY
 
   //MARS CURIOSITY API ADDRESS---https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2020-6-3&api_key=DEMO_KEY
 
+  const [image, setImage] = useState();
+
   function handleFetch() {
     fetch(url)
       .then((res) => res.json())
       .then((data) => {
-        // setImage(req.body.resource.url);
+        setImage(data.url);
         console.log(data);
       });
   }
 
   return (
     <div>
-      <p>Welcome from NASA</p>
+      <p>You're being watched by NASA</p>
       <br />
-      <button onClick={handleFetch}>Click me</button>
+      <button onClick={handleFetch}>I can see my house from here</button>
       <hr />
       <br />
+      <img width="75%" height="75%" src={image} alt="Satellite Location" />
       <div>
         <p>The date of this image capture is from: {date}</p>
       </div>
